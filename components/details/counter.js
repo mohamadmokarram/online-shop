68e73ee9b040b3a2c623eddc012def582cd05339
Mohamad <139718328+mohamadmokarram@users.noreply.github.com>
2025-09-04 18:02:25 +0330

    changing arrow up & down icons in counter component

--- components/details/counter.js
+++ components/details/counter.js
@@ -1,22 +1,21 @@
-import Icon from "../icon";
 import { ToPersianNumber } from "topersiannumber";
+import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 
 export default function Counter({ id, count, handleCountUp, handleCountDown, classes }) {
   return (
     <div className={`flex flex-col items-center ${classes}`}>
-      <Icon
+      <IoIosArrowUp
         onClick={() => handleCountUp(id)}
-        title="keyboard_arrow_up"
-        classes="h-7 text-[#b6b6b6] duration-200 hover:text-textColor select-none !text-3xl"
+        className="h-7 text-[#b6b6b6] duration-200 hover:text-textColor select-none !text-3xl"
       />
       <span className="text-[#b6b6b6] text-[13px]">
         {ToPersianNumber(count)}
       </span>
-      <Icon
+      <IoIosArrowDown
         onClick={() => handleCountDown(id)}
-        title="keyboard_arrow_down"
-        classes="h-7 text-[#b6b6b6] duration-200 hover:text-textColor select-none !text-3xl -mt-2"
+        className="h-7 text-[#b6b6b6] duration-200 hover:text-textColor select-none !text-3xl -mt-2"
       />
     </div>
   );
 }
+
